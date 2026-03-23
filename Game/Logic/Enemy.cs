9453f7bd3d4d20966ebce3c294b4065bf718350f
Matt6049 <mati6049@gmail.com>
2026-03-23 14:44:25 +0100

    Create placeholder enemy with pathfinding through the provided path.

---
+++ Game/Logic/Enemy.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Enemy : AnimatableBody2D
+{
+	const float Speed = 20;
+	List<Vector2> Path { get; set; }
+	int PathIndex { get; set; }
+
+	public override void _Ready() {
+		base._Ready();
+	}
+
+	public void ConstructEnemy(Node parent, List<Vector2> path) {
+		this.Position = path[0];
+		this.Path = path;
+		parent.AddChild(this);
+	}
+
+	
+	public override void _PhysicsProcess(double delta) {
+		base._PhysicsProcess(delta);
+		if(PathIndex >= Path.Count-1) 
+			return;
+		Position = Position.MoveToward(Path[PathIndex + 1], Speed*(float)delta);
+
+		if (Position.IsEqualApprox(Path[PathIndex + 1]))
+			PathIndex++;
+	}
+}
