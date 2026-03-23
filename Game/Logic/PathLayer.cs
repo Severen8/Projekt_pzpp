8f12f14e5938113b133c20c2e042c3d42db213f9
Matt6049 <mati6049@gmail.com>
2026-03-23 14:43:46 +0100

    Add Path getter to TileLayer. Change the name of Corners property to Vertices.

--- Game/Logic/PathLayer.cs
+++ Game/Logic/PathLayer.cs
@@ -3,43 +3,50 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 public partial class PathLayer : TileMapLayer
 {
 	List<Vector2I> OrderedNodes { get; set; }
-	List<Vector2I> Corners { get; set; }
+	List<Vector2I> Vertices { get; set; }
 
 	public override void _Ready() {
 		base._Ready();
-		OrderedNodes = [this.GetUsedCellsById(0, new Vector2I(0, 0), 0).First()];
-		Corners = [.. OrderedNodes];
-		this.ConstructGraph();
+		OrderedNodes = [GetUsedCellsById(0, new Vector2I(0, 0), 0).First()];
+		Vertices = [.. OrderedNodes];
+		ConstructGraph();
 	}
 
 	private void ConstructGraph() {
 		HashSet<Vector2I> visited = new();
-		Vector2I previous = this.OrderedNodes[0];
-		Vector2I nextDir = this.GetSurroundingCells(previous).FirstOrDefault(previous) - previous;
+		Vector2I previous = OrderedNodes[0];
+		Vector2I nextDir = GetSurroundingCells(previous).FirstOrDefault(previous) - previous;
 		visited.Add(previous);
 
 		while(nextDir != Vector2I.Zero) {
 			previous = PopulateGraphInDirection(previous, nextDir);
-			nextDir = this.GetSurroundingCells(previous)
-				.Where(vec => vec != OrderedNodes[OrderedNodes.Count - 2] && this.GetCellSourceId(vec) != -1)
+			nextDir = GetSurroundingCells(previous)
+				.Where(vec => vec != OrderedNodes[^2] && GetCellSourceId(vec) != -1)
 				.FirstOrDefault(previous)
 				- previous;
-			Corners.Add(previous);
-		}
-		foreach(Vector2I corner in Corners) {
-			GD.Print(corner);
+			Vertices.Add(previous);
 		}
 	}
 
 	private Vector2I PopulateGraphInDirection(Vector2I origin, Vector2I direction) {
 		Vector2I next = origin;
-		while(this.GetCellSourceId(next+=direction) != -1) {
+		while(GetCellSourceId(next+=direction) != -1) {
 			OrderedNodes.Add(next);
 		}
 		return next - direction;
+	}
+
+	public List<Vector2> GetPath() {
+		Vector2 TileOffset = new Vector2(this.TileSet.TileSize.X, this.TileSet.TileSize.Y) * 0.5f;
+		return this.Vertices
+			.Select(vert => 
+				Position + new Vector2(vert.X * this.TileSet.TileSize.X, vert.Y * this.TileSet.TileSize.Y) 
+				+ TileOffset)
+			.ToList();
 	}
 }
