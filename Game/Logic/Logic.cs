e260be3bb5353ec1be31fe371a1d3ae8ea0cfcf5
Matt6049 <mati6049@gmail.com>
2026-03-23 14:44:45 +0100

    Create Logic script for spawning a test enemy.

---
+++ Game/Logic/Logic.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Logic : Node2D
+{
+	static readonly PackedScene EnemyScene = ResourceLoader.Load<PackedScene>("res://Game/Logic/Enemy.tscn");
+
+	List<Vector2> Path { get; set; }
+	Enemy MonsterInstance { get; set; }
+	
+	public override void _Ready() {
+		base._Ready();
+		Path = GetNode<PathLayer>("PathLayer").GetPath();
+		MonsterInstance = EnemyScene.Instantiate<Enemy>();
+		MonsterInstance.ConstructEnemy(this, Path);
+	}
+}
