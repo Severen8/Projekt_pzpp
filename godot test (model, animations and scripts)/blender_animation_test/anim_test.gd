cb31dd56435991dd58ee6e0cb0b9980f4ec5769d
DESKTOP-QAFVCTU\Eryk Przybyła <ded.vin@vp.pl>
2026-03-30 13:32:08 +0200

    Added animations for the rest of the balista towers, ported them to godot, and made a scene that has all tiers and has function for swtiching between them. Signed-off-by: DESKTOP-QAFVCTU\Eryk Przybyła <ded.vin@vp.pl>

--- godot test (model, animations and scripts)/blender_animation_test/anim_test.gd
+++ godot test (model, animations and scripts)/blender_animation_test/anim_test.gd
@@ -8,18 +8,22 @@ var dir = 0
 func _physics_process(delta):
 	$Marker3D.rotate_y(0.001)
 	if Input.is_action_pressed("spawn_anim"):
-		$Ballista_T1_test.play_anim("spawn")
+		$Ballistas.play_anim("spawn")
 	elif Input.is_action_pressed("shoot_anim"):
-		$Ballista_T1_test.play_anim("shoot", 2)
+		$Ballistas.play_anim("shoot", 2)
 	elif Input.is_action_pressed("reload_anim"):
-		$Ballista_T1_test.play_anim("reload", 0.5)
+		$Ballistas.play_anim("reload", 0.5)
 	if Input.is_action_just_pressed("rotate_left"):
 		dir += 45
 		if dir > 360:
 			dir -= 360
-		$Ballista_T1_test.dir_rot(dir)
+			if $Ballistas.get_meta("Tier") < 4:
+				$Ballistas.update_tier($Ballistas.get_meta("Tier")+1)
+		$Ballistas.dir_rot(dir)
 	if Input.is_action_just_pressed("rotate_right"):
 		dir -= 45
 		if dir < 0:
 			dir += 360
-		$Ballista_T1_test.dir_rot(dir)
+			if $Ballistas.get_meta("Tier") > 1:
+				$Ballistas.update_tier($Ballistas.get_meta("Tier")-1)
+		$Ballistas.dir_rot(dir)
