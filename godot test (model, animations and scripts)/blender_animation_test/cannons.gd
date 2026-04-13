d6d029dcfa6de326e3c4ddaf993e9be8a7241516
DESKTOP-QAFVCTU\Eryk Przybyła <ded.vin@vp.pl>
2026-04-13 16:46:15 +0200

    Added animations to cannons, and ported them to godot scene (with code to control them) Signed-off-by: DESKTOP-QAFVCTU\Eryk Przybyła <ded.vin@vp.pl>

--- godot test (model, animations and scripts)/blender_animation_test/cannons.gd
+++ godot test (model, animations and scripts)/blender_animation_test/cannons.gd
@@ -11,11 +11,11 @@ func _ready():
 
 func play_anim(v, s = 1.0):
 	if(v == "spawn"):
-		selected.get_node("AnimationPlayer").play_section("Scene",0,1.5, -1, s)
+		selected.get_node("AnimationPlayer").play_section("Scene",0,1.21, -1, s)
+	elif(v == "ready"):
+		selected.get_node("AnimationPlayer").play_section("Scene",2.445,3.1, -1, s)
 	elif(v == "shoot"):
-		selected.get_node("AnimationPlayer").play_section("Scene",1.8,2.3, -1, s)
-	elif(v == "reload"):
-		selected.get_node("AnimationPlayer").play_section("Scene",2.65,3.23, -1, s)
+		selected.get_node("AnimationPlayer").play_section("Scene",3.45,3.7083, -1, s)
 	elif(v == "reset"):
 		selected.get_node("AnimationPlayer").play("Scene", -1, 0.0001)
 		selected.get_node("AnimationPlayer").stop()
@@ -25,10 +25,10 @@ func play_anim(v, s = 1.0):
 func dir_rot(a):
 	var num = str(self.get_meta("Tier"))
 	var not_rotate_parts = [
-		selected.get_node_or_null("T"+num+"PlatformBase"),
-		selected.get_node_or_null("T"+num+"TowerBase"),
-		selected.get_node_or_null("T"+num+"TowerFoot"),
-		selected.get_node_or_null("T"+num+"Reinforcements")
+		selected.get_node_or_null("C"+num+"PlatformBase"),
+		selected.get_node_or_null("C"+num+"TowerBase"),
+		selected.get_node_or_null("C"+num+"TowerFoot"),
+		selected.get_node_or_null("C"+num+"Reinforcements")
 		]
 	var def = self.rotation_degrees
 	def.y = a
@@ -42,7 +42,7 @@ func dir_rot(a):
 func update_tier(t = -1):
 	if t != -1:
 		self.set_meta("Tier",t)
-	selected = get_node("T" + str(self.get_meta("Tier")))
+	selected = get_node("C" + str(self.get_meta("Tier")))
 	for i in get_children():
 		if str(i).substr(1,1) != str(self.get_meta("Tier")):
 			i.hide()
