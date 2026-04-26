b761721095668ddc1dcc69beae288e919926ee6f
Matt6049 <mati6049@gmail.com>
2026-04-26 18:21:28 +0200

    Move Enemies to a dedicated directory

--- Game/Logic/Enemies/Enemy.cs
+++ Game/Logic/Enemies/Enemy.cs
@@ -4,25 +4,29 @@ using System.Collections.Generic;
 
 public partial class Enemy : AnimatableBody2D
 {
+	private static readonly PackedScene PackedScene = ResourceLoader.Load<PackedScene>("res://Game/Logic/Enemies/Enemy.tscn");
 	const float Speed = 20;
 	List<Vector2> Path { get; set; }
 	int PathIndex { get; set; }
 
-	public override void _Ready() {
-		base._Ready();
+
+	public static Enemy Load() {
+		return PackedScene.Instantiate<Enemy>();
 	}
 
-	public void ConstructEnemy(Node parent, List<Vector2> path) {
+
+
+	public void Spawn(Node parent, List<Vector2> path) {
 		this.Position = path[0];
 		this.Path = path;
-		parent.AddChild(this);
+		parent.CallDeferred("add_child", this);
 	}
 
 	
 	public override void _PhysicsProcess(double delta) {
 		base._PhysicsProcess(delta);
 		if (PathIndex >= Path.Count - 1) {
-			Free();
+			Free(); //todo: fire event instead, handle this in the parent
 			return;
 		}
 		Position = Position.MoveToward(Path[PathIndex + 1], Speed*(float)delta);
