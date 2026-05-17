46938e6d560cbd5897e794a470503d9725f095dc
Matt6049 <mati6049@gmail.com>
2026-05-17 17:33:20 +0200

    Change Golem scene to use CompositeEnemy node type. Fix audio path finding.

--- Game/Enemies/EnemySim.cs
+++ Game/Enemies/EnemySim.cs
@@ -3,7 +3,11 @@ using System;
 using System.Collections.Generic;
 
 namespace MedievalTDIncremental.Game.Enemies {
+	[GlobalClass]
 	public partial class EnemySim : AnimatableBody2D {
 		//todo: collision checks
+		public override void _Ready() {
+			base._Ready();
+		}
 	}
 }
