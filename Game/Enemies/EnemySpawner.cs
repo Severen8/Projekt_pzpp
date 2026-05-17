cced206a2b609b358e1c871b34172d514a35f38b
Matt6049 <mati6049@gmail.com>
2026-05-17 16:34:06 +0200

    Modify EnemySpawner.cs to use composite enemies. Add method for manually adding new enemies.

--- Game/Enemies/EnemySpawner.cs
+++ Game/Enemies/EnemySpawner.cs
@@ -1,18 +1,27 @@
+using Godot;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MedievalTDIncremental.Game.Enemies {
 	public static class EnemySpawner {
-		//todo: maybe move this into EnemyHandler?
-		private static readonly Dictionary<string, Func<CompositeEnemy>> StringToEnemy = new() {
-			
-		};
+		private const string ENEMIES_ROOT = "res://Game/Enemies/";
+		private static readonly Dictionary<string, PackedScene> StringToEnemy;
+
+		static EnemySpawner() {
+			AddNewEnemy("Golem");
+		}
+
+		private static void AddNewEnemy(string enemyName) {
+			StringToEnemy.Add(enemyName, ResourceLoader.Load<PackedScene>($"{ENEMIES_ROOT}{enemyName}/{enemyName}.tscn"));
+		}
+
 
 		public static CompositeEnemy FromString(string EnemyType) {
-			return StringToEnemy[EnemyType]();
+			return StringToEnemy[EnemyType].Instantiate<CompositeEnemy>();
 		}
 	}
 }
